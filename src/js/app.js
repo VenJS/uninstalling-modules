9484f8a1caf24f7391cd027f086ebf7d3ca58908
Venci <venci34@gmail.com>
2022-01-04 14:19:05 +0200

    should be done

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 import "../scss/app.scss";
-const R = require("ramda");
+// const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const isEven = numbersArray.filter((n) => n % 2 === 0);
+  for (let i = 0; i <= isEven.length; i++) {
+    const element = isEven[i];
+    let li = document.createElement("li");
+    li.textContent = element;
+    ul.appendChild(li);
+  }
 });
